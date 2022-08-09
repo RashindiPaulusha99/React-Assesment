601672dfd226d7f717c2e41f7d9c6831a5f30674
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2022-08-09 22:16:46 +0530

    add submit button user registration page

--- src/pages/Session/Register/index.jsx
+++ src/pages/Session/Register/index.jsx
@@ -5,9 +5,17 @@ import Grid from '@mui/material/Grid';
 import Typography from "@mui/material/Typography";
 import GDSEButton from "../../../components/Home/Common/Button";
 import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 
 class UserRegister extends Component {
+
     constructor(props) {
         super(props);
 
@@ -27,15 +35,25 @@ class UserRegister extends Component {
                 mobileNo:""
             }
         }
-    }
 
-    handleSubmit = () => {
-        console.log("handle submit");
-        console.log(this.state.formData);
+        function createData(name, calories, fat, carbs, protein) {
+            return { name, calories, fat, carbs, protein };
+        }
+
+        this.state={
+            rows : [
+                createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+                createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+                createData('Eclair', 262, 16.0, 24, 6.0),
+                createData('Cupcake', 305, 3.7, 67, 4.3),
+                createData('Gingerbread', 356, 16.0, 49, 3.9),
+            ]
+        }
+
     }
 
     render() {
-        const {classes} = this.props;
+
         return (
             <Fragment>
                 <ValidatorForm
@@ -214,7 +232,35 @@ class UserRegister extends Component {
                             </div>
                         </Grid>
                         <Grid item lg={12} md={12} sm={12} xm={12}>
-
+                            <TableContainer component={Paper}>
+                                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>Dessert (100g serving)</TableCell>
+                                            <TableCell align="right">Calories</TableCell>
+                                            <TableCell align="right">Fat(g)</TableCell>
+                                            <TableCell align="right">Carbs(g)</TableCell>
+                                            <TableCell align="right">Protein(g)</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {this.state.rows.map((row) => (
+                                            <TableRow
+                                                key={row.name}
+                                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                            >
+                                                <TableCell component="th" scope="row">
+                                                    {row.name}
+                                                </TableCell>
+                                                <TableCell align="right">{row.calories}</TableCell>
+                                                <TableCell align="right">{row.fat}</TableCell>
+                                                <TableCell align="right">{row.carbs}</TableCell>
+                                                <TableCell align="right">{row.protein}</TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
                         </Grid>
                     </Grid>
                 </ValidatorForm>
